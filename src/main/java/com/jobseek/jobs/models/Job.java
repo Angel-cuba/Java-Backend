78455942eeef4775acedf5ee6171942f6ef6d95a
Mac_Developer <araozangel842@gmail.com>
2023-03-07 21:49:30 +0200

    Cleaning code

--- src/main/java/com/jobseek/jobs/models/Job.java
+++ src/main/java/com/jobseek/jobs/models/Job.java
@@ -20,36 +20,4 @@ public class Job {
   private String desc;
   private int exp;
   private List<String> techs;
-
-public String getId() {
-  return _id;
-}
-public void setId(String id) {
-  this._id = id;
-}
-public String getProfile() {
-  return profile;
-}
-public void setProfile(String profile) {
-  this.profile = profile;
-}
-public String getDesc() {
-  return desc;
-}
-public void setDesc(String desc) {
-  this.desc = desc;
-}
-public int getExp() {
-  return exp;
-}
-public void setExp(int exp) {
-  this.exp = exp;
-}
-public List<String> getTechs() {
-  return techs;
-}
-public void setTechs(List<String> techs) {
-  this.techs = techs;
-}
-
 }
